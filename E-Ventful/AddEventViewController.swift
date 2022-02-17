fa85754c7760acb62b1c0e28e5175638d2c293a4
Drake Neuenschwander <drakeneuenschwander@gmail.com>
2022-02-17 13:37:53 -0600

    working calendar

--- E-Ventful/AddEventViewController.swift
+++ E-Ventful/AddEventViewController.swift
@@ -9,19 +9,56 @@ import UIKit
 
 class AddEventViewController: UIViewController {
 
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var fromDate: UITextField!
+    
+    
+    @IBOutlet weak var fromDateTxt: UITextField!
     @IBOutlet weak var toDate: UITextField!
+    var toolBar = UIToolbar()
+    var dateFormatter = DateFormatter()
+    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd/MM/yyyy"
-        fromDate.text = formatter.string(from:datePicker.date)
-    }
-    
+        
+        fromDateTxt.inputView = datePicker
+        toDate.inputView = datePicker
+        
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
+        toolBar.setItems([doneButton] , animated: true)
+        fromDateTxt.inputAccessoryView = toolBar
+        toDate.inputAccessoryView = toolBar
+        
 
+      
+                                         }
+    
+    func createDatePickerView(_ textField: UITextField){
+        
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        if textField == fromDateTxt
+            {
+            datePicker.datePickerMode = .date
+        }
+        if textField == toDate
+            {
+            datePicker.datePickerMode = .date
+        }
+    }
+    @objc func doneButtonTapped() {
+        if fromDateTxt.isFirstResponder {
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .none
+            fromDateTxt.text = dateFormatter.string(from: datePicker.date)
+        }
+        if toDate.isFirstResponder {
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .none
+            toDate.text = dateFormatter.string(from: datePicker.date)
+        }
+        self.view.endEditing(true)
+    }
 
 }
